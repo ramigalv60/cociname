d525ba9ca9650b68e33980d756cfc620832acc74
ramigalv60 <104180859+ramigalv60@users.noreply.github.com>
2023-11-30 00:46:30 -0300

    Refactor publicRoutes in authMiddleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
-  publicRoutes: ["/", "/api/trpc/recipesRouter/getRecipeBySearch", "/api/trpc/recipesRouter/getRecetaByCategory", "/api/trpc/getRouters/getRecipes", "/api/trpc/getRouters/getVideos", "/api/trpc/getRouters/getIngredients", "/api/trpc/getRouters/getCategories", "/api/trpc/categoriasRouters/createCategoria", "/api/trpc/categoriasRouters/updateCategoria", "/api/trpc/categoriasRouters/deleteCategoria"],
+  publicRoutes: ["/", "/(api|trpc)(.*)"],
 });
  
 export const config = {
