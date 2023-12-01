ecefa8106fc0914805656d988656087004e408dd
ramigalv60 <104180859+ramigalv60@users.noreply.github.com>
2023-11-30 22:02:56 -0300

    Added useful comment for the middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // "/" will be accessible to all users
+  // "/(api|trpc)(.*)" will be accessible to all usersq
   publicRoutes: ["/", "/(api|trpc)(.*)"],
 });
  
